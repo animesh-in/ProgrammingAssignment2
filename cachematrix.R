64a62f02f6fcbbe64c2934ed0f0b360b54f5987f
animesh-in <47326650+animesh-in@users.noreply.github.com>
2019-02-04 22:47:57 +0530

    Add files via upload

--- cachematrix.R
+++ cachematrix.R
@@ -2,14 +2,39 @@
 ## functions do
 
 ## Write a short comment describing this function
-
+## This function creates a special "matrix" object that can cache its inverse
 makeCacheMatrix <- function(x = matrix()) {
+        
+        invMatrix <- NULL
+
+        setMatrix <- function(y) { 
+                x <<- y 
+                invMatrix <<- NULL 
+        }
+
+        get <- function() x 
+        setMatrix <- function(inverse) invMatrix <<- inverse 
+        getinverse <- function() invMatrix 
+        list(setMatrix = setMatrix, getMatrix = getMatrix, setInverse = setInverse, getInverse = getInverse) 
 
 }
 
 
 ## Write a short comment describing this function
+## This function computes the inverse of the special "matrix" returned by makeCacheMatrix above. 
+## If the inverse has already been calculated (and the matrix has not changed), 
+## then the cachesolve should retrieve the inverse from the cache.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+
+        invMatrix <- x$getInverse()
+        if(!is.null(invMatrix)) { 
+                message("Getting Cached Data") 
+                return(invMatrix) 
+        }
+
+        dataMatrix <- x$getMatrix() 
+        invMatrix <- solve(dataMatrix, ...) 
+        x$setInverse(invMatrix) 
+        invMatrix
 }
